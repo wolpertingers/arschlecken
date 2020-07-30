958612ed95dfecbd5935321d8130a1bad6f79e10
Ingo Kuba <ingo.kuba@yahoo.de>
2020-07-30 22:36:56 +0200

    asddasds

--- code.js
+++ code.js
@@ -1,5 +1,17 @@
 document.addEventListener("click", getPoints);
 
+
+$(document).ready(function() {
+    $("#points").hide();
+});
+
 function getPoints(event) {
     console.log("x: " + event.clientX + ", y: " + event.clientY)
+    showPoints(100);
+}
+
+function showPoints(points) {
+    var element = $("#points");
+    element.html(`+${points}`)
+    element.slideToggle(1000, () => element.slideToggle(1000));
 }
