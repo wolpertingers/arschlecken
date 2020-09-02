61f7fabc2deac7ca85440243b7ce719e47812e01
Ingo <ikuba@intension.de>
2020-09-02 17:14:46 +0200

    Use fabric.js to handle clicks on canvas.

--- code.js
+++ code.js
@@ -1,17 +1,60 @@
-document.addEventListener("click", getPoints);
+var canvas, scoreText;
+var score = 0;
 
+function init() {
+	canvas = new fabric.Canvas('canvas', { selection: false });
+	scoreText = new fabric.Text('Punkte: 0', {
+		fontFamily: 'Helvetica, Arial, sans-serif',
+		fontSize: 50,
+		selectable: false,
+		evented: false,
+		left: 10,
+		top: 10
+	});
+	canvas.add(scoreText);
 
-$(document).ready(function() {
-    $("#points").hide();
-});
+	// resize canvas to window size
+	window.addEventListener('resize', resizeCanvas, false);
+	resizeCanvas();
 
-function getPoints(event) {
-    console.log("x: " + event.clientX + ", y: " + event.clientY)
-    showPoints(100);
+	// on click event
+	canvas.on('mouse:down', function(options) {
+		let x = canvas.width / options.pointer.x;
+		let y = canvas.height / options.pointer.y;
+		let distX = Math.pow(2.14 - x, 2);
+		let distY = Math.pow(1.3 - y, 2);
+		let dist = distX + distY;
+		let c = Math.sqrt(dist);
+		let tp = 1 - c;
+		if (tp > 0) {
+			let points = Math.ceil(tp * 100);
+			let text = new fabric.Text(`+${points}`, {
+				fontFamily: 'Helvetica, Arial, sans-serif',
+				fontSize: 100,
+				fill: 'darkorange',
+				strokeWidth: 3,
+				stroke: 'orangered',
+				selectable: false,
+				evented: false,
+				left: options.pointer.x,
+				top: options.pointer.y
+			});
+			text.animate('opacity', '0', {
+				duration: 1000,
+				onChange: canvas.renderAll.bind(canvas),
+				onComplete: function() {
+					canvas.remove(text);
+				}
+			});
+			canvas.add(text);
+			score += points;
+			scoreText.text = `Punkte: ${score}`;
+		}
+	});
 }
 
-function showPoints(points) {
-    var element = $("#points");
-    element.html(`+${points}`)
-    element.slideToggle(1000, () => element.slideToggle(1000));
+function resizeCanvas() {
+	canvas.setHeight(window.innerHeight * 0.9);
+	canvas.setWidth(window.innerWidth);
+	canvas.renderAll();
 }
